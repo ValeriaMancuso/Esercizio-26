87375bb34edfd7e5eddf5ccd2634df5e40112874
Valeria Mancuso <valeriamancuso5@icloud.com>
2022-11-23 16:04:06 +0100

    fine

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-var array = [];
-function getData() {
-    fetch('http://localhost:3000/abbigliamento').then(function (response) {
-        return response.json;
-    }).then(function (data) {
-        array = data;
-        console.log(array);
-    });
-}
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Abbigliamento = /** @class */ (function () {
     function Abbigliamento(_id, _codprod, _collezione, _capo, _modello, _quantita, _colore, _prezzoivaesclusa, _prezzoivainclusa, _disponibile, _saldo) {
         this.id = _id;
@@ -22,10 +28,32 @@ var Abbigliamento = /** @class */ (function () {
         this.saldo = _saldo;
     }
     Abbigliamento.prototype.getSaldoCapo = function () {
-        return (this.prezzoivaesclusa * this.saldo) / 100;
+        return (this.prezzoivainclusa * this.saldo) / 100;
     };
     Abbigliamento.prototype.getAcquistoCapo = function () {
         return this.prezzoivainclusa - this.getSaldoCapo();
     };
     return Abbigliamento;
 }());
+var Capo = /** @class */ (function (_super) {
+    __extends(Capo, _super);
+    function Capo(_id, _codprod, _collezione, _capo, _modello, _quantita, _colore, _prezzoivaesclusa, _prezzoivainclusa, _disponibile, _saldo) {
+        return _super.call(this, _id, _codprod, _collezione, _capo, _modello, _quantita, _colore, _prezzoivaesclusa, _prezzoivainclusa, _disponibile, _saldo) || this;
+    }
+    return Capo;
+}(Abbigliamento));
+var array = [];
+getData();
+function getData() {
+    fetch('http://localhost:3000/capi').then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        array = data;
+        console.log(array);
+        array.map(function (element) {
+            var capo = new Capo(element.id, element.codprod, element.collezione, element.capo, element.modello, element.quantita, element.colore, element.prezzoivaesclusa, element.prezzoivainclusa, element.disponibile, element.saldo);
+            console.log(capo);
+            console.log('Totale capo: ', capo.getAcquistoCapo(), '€');
+        });
+    });
+}
